4dcd713dc2516262c498aff38afbf3095395eb3f
Matej Kujnisch <matej.kujnisch@gmail.com>
2024-09-30 14:52:18 +0200

    start

---
+++ P2PShare.Libs/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+namespace P2PShare.Libs
+{
+    public class Connection
+    {
+        //async TcpClient Connect(string ip)
+        //{
+        //    TcpClient client = new TcpClient();
+        //}
+    }
+}
--- P2PShare/Program.cs
+++ P2PShare/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Net.Sockets;
+
+namespace P2PShare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("IP addresss: ");
+
+            //string ip = Console
+            
+            Console.ReadKey();
+        }
+    }
+}
