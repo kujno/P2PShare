f68e5cfa05edd2703ea3ffd37f19ec05fa6ded0f
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-01-05 18:19:09 +0100

    background & close, minimize icons

---
+++ P2PShare.GUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P2PShare.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
