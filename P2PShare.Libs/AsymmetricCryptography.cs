2830376987a6c4a64df9bc8895007e248879dcac
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-04-08 20:52:41 +0200

    symmetric and asymmetric cryptography methods

---
+++ P2PShare.Libs/AsymmetricCryptography.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+
+namespace P2PShare.Libs
+{
+    class AsymmetricCryptography
+    {
+        public static RSAParameters[] GenerateKeys()
+        {
+            RSAParameters[] parameters = new RSAParameters[2];
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+            {
+                // public key
+                parameters[0] = rsa.ExportParameters(false);
+                //private key
+                parameters[1] = rsa.ExportParameters(true);
+            }
+
+            return parameters;
+        }
+
+        public static byte[] Encrypt(byte[] originalData, RSAParameters key)
+        {
+            byte[] encryptedData;
+
+            using (RSA rsa = RSA.Create(key))
+            {
+                encryptedData = rsa.Encrypt(originalData, RSAEncryptionPadding.OaepSHA256);
+            }
+
+            return encryptedData;
+        }
+
+        public static byte[] Decrypt(byte[] encryptedData, RSAParameters key)
+        {
+            byte[] decryptedData;
+            
+            using (RSA rsa = RSA.Create(key))
+            {
+                decryptedData = rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
+            }
+            
+            return decryptedData;
+        }
+    }
+}
---
+++ P2PShare.Libs/SymmetricCryptography.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+
+namespace P2PShare.Libs
+{
+    class SymmetricCryptography
+    {
+        public static byte[] Encrypt(byte[] data, byte[] key, byte[] nonce)
+        {
+            AesGcm aes;
+            byte[] encryptedData;
+            byte[] tag = new byte[16];
+
+            do
+            {
+                aes = new (key, 16);
+            }
+            while (aes.TagSizeInBytes is null);
+
+            encryptedData = new byte[data.Length + (int)aes.TagSizeInBytes];
+
+            aes.Encrypt(nonce, data, encryptedData, tag);
+
+            Array.Copy(tag, 0, encryptedData, data.Length, tag.Length);
+
+            return encryptedData;
+        }
+
+        public static byte[] Decrypt(byte[] data, byte[] key, byte[] nonce)
+        {
+            AesGcm aes;
+            byte[] tag;
+            byte[] decryptedData;
+
+            do
+            {
+                aes = new(key, 16);
+            }
+            while (aes.TagSizeInBytes is null);
+
+            tag = new byte[(int)aes.TagSizeInBytes];
+            decryptedData = new byte[data.Length - tag.Length];
+
+            Array.Copy(data, data.Length - tag.Length, tag, 0, tag.Length);
+
+            aes.Decrypt(nonce, data, tag, decryptedData);
+
+            return decryptedData;
+        }
+    }
+}
