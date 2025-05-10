7a6dd80d03f495726f03096293cdae8b448f1e35
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-05-11 01:09:44 +0200

    900

--- P2PShare.Libs/FileTransport.cs
+++ P2PShare.Libs/FileTransport.cs
@@ -161,7 +161,7 @@ namespace P2PShare.Libs
 
                 await stream.ReadAsync(buffer, 0, buffer.Length);
                 aesKey = decryptographer.Decrypt(buffer);
-                await Task.Delay(800);
+                await Task.Delay(900);
                 EncryptionSymmetrical encryption = new(aesKey);
 
                 await FileHandling.CreateFile(stream, filePath, fileLength, encryption);
