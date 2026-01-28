5320d7c20f112f0cf2a96f2d410940c1a0c191a3
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-01-29 00:07:32 +0100

    ProgressBar

--- CustomMessageBox.xaml.cs
+++ CustomMessageBox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using P2PShare.Models;
+﻿using P2PShare.Libs.Models;
+using P2PShare.Models;
 using System.Windows;
 using System.Windows.Input;
 
@@ -12,14 +13,14 @@ namespace P2PShare
         private readonly ButtonContent _buttonContent;
 
         public event EventHandler<bool>? WindowClosed;
-        
-        public void ChangeContent(string content) => Text.Text = content;
+
+        public CustomMessageBox(FilePartTransportedEventArgs transportInfo, KeyValuePair<string, long> file, Window window) : this(String.Empty, ButtonContent.Cancel, window) => ChangeContent(transportInfo, file);
 
         public CustomMessageBox(string content, ButtonContent buttonContent, Window window)
         {
             InitializeComponent();
 
-            Text.Text = content;
+            TextBlock_File.Text = content;
             _buttonContent = buttonContent;
             Btn.Content = buttonContent;
             if (window.Dispatcher.CheckAccess())
@@ -27,6 +28,16 @@ namespace P2PShare
                 Owner = window;
                 Owner.Visibility = Visibility.Hidden;
             }
+        }
+
+        private void OnWindowClosed() => WindowClosed?.Invoke(this, _buttonContent is ButtonContent.Cancel ? true : false);
+
+        public void ChangeContent(FilePartTransportedEventArgs transportInfo, KeyValuePair<string, long> file)
+        {
+            TextBlock_File.Text = $"{(transportInfo.SendReceive is SendReceive.Send ? "Sending" : "Receiving")}: {file.Key} {file.Value}B ({transportInfo.CurrentFile}/{transportInfo.AmountOfFiles})";
+            if (Grid_Progress.Visibility is not Visibility.Visible) Grid_Progress.Visibility = Visibility.Visible;
+            ProgressBar.Value = transportInfo.Part;
+            TextBlock_Percentage.Text = $"{transportInfo.Part}%";
         }
 
         private void Btn_Click(object sender, RoutedEventArgs e)
@@ -39,11 +50,6 @@ namespace P2PShare
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left) DragMove();
-        }
-
-        private void OnWindowClosed()
-        {
-            WindowClosed?.Invoke(this, _buttonContent == ButtonContent.Cancel ? true : false);
         }
     }
 }
