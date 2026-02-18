405c1db53852b48a0cdb1aee2c8d3d08ad1543d3
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-18 22:02:03 +0100

    LoginWindow

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace P2PShare
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
