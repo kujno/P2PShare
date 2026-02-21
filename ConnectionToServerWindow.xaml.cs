a9b642b4d6ae849c5dbfb133eeeba344757747a5
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-21 19:21:55 +0100

    ConnectionToServerWindow

---
+++ ConnectionToServerWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using P2PShare.Connection;
+using System.Net;
+using System.Windows;
+using System.Windows.Input;
+
+namespace P2PShare
+{
+    /// <summary>
+    /// Interaction logic for ConnectionToServerWindow.xaml
+    /// </summary>
+    public partial class ConnectionToServerWindow : Window
+    {
+        public ConnectionToServerHandler? ConnectionHandler { get; private set; } = null;
+
+        private Task? _connecting;
+        private CancellationTokenSource? _cts;
+        private IPAddress _serverIP;
+
+        public ConnectionToServerWindow(IPAddress serverIP, Window window)
+        {
+            InitializeComponent();
+            if (window.Dispatcher.CheckAccess())
+                Owner = window;
+            _serverIP = serverIP;
+            _connecting = ConnectAsync();
+        }
+
+        private void TryAgain_Click(object sender, RoutedEventArgs e) => _connecting = ConnectAsync();
+
+        private async void ChangeIP_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress? newIP;
+
+            ServerIPWindow ipWindow = new("Cancel");
+            ipWindow.ShowDialog();
+            if (_serverIP != (newIP = await ServerIP.GetAsync()))
+            {
+                _serverIP = newIP!;
+                _connecting = ConnectAsync();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) DragMove();
+        }
+
+        private async Task ConnectAsync()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            ConnectionHandler?.Dispose();
+
+            _cts = new();
+            ConnectionHandler = new()
+            {
+                CancellationToken = _cts.Token,
+                IPLocal = IPAddress.Any,
+                IPServer = _serverIP
+            };
+
+            Text.Text = $"Connecting to {ConnectionHandler.IPServer}...";
+
+            try
+            {
+                await ConnectionHandler.ConnectAsync();
+            }
+            catch
+            {
+                Text.Text = $"Failed to connect.";
+
+                return;
+            }
+
+            Close();
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            ConnectionHandler?.Dispose();
+            ConnectionHandler = null;
+
+            Close();
+        }
+    }
+}
