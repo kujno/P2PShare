651d64880e5cafb4b989bd19dfd8399ddd6f2b4f
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-21 19:20:05 +0100

    AppHelper static class

---
+++ Utils/AppHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace P2PShare.Utils
+{
+    public static class AppHelper
+    {
+        public static void CloseAppForServer(Window window)
+        {
+            new CustomMessageBox("Disconnected from server.\nRestart the application.", Models.ButtonContent.OK, window).ShowDialog();
+
+            Application.Current.Shutdown();
+        }
+    }
+}
