cc06f327ad4c86c3715ef8d077cd2b5dbff97a49
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-21 15:54:50 +0100

    ServerIP static class

---
+++ ServerIP.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Net;
+
+namespace P2PShare
+{
+    public static class ServerIP
+    {
+        private static readonly string filePath = "ServerIP.txt";
+
+        public static async Task Set(IPAddress ip) => await File.WriteAllTextAsync(filePath, ip.ToString());
+
+        public static async Task<IPAddress?> Get()
+        {
+            IPAddress? ip;
+
+            if (File.Exists(filePath)
+                && IPAddress.TryParse(await File.ReadAllTextAsync(filePath), out ip))
+            {
+                return ip;
+            }
+            else return null;
+        }
+    }
+}
