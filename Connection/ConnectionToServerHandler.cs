b9cf9e1db18e3153022991631160305c61fae24d
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-21 19:21:26 +0100

    ConnectionToServerHandler class

---
+++ Connection/ConnectionToServerHandler.cs
@@ -0,0 +1,38 @@
+﻿using P2PShare.Libs;
+using System.Net;
+using System.Net.Sockets;
+
+namespace P2PShare.Connection
+{
+    public class ConnectionToServerHandler : ConnectionHandler
+    {
+        public event EventHandler? Disconnected;
+
+        public required IPAddress IPServer
+        {
+            get
+            {
+                return _ipRemote
+                    ?? throw new ArgumentNullException();
+            }
+
+            set => _ipRemote = value;
+        }
+
+        public async Task ConnectAsync() => Client = await ConnectAsync(_initialServerPort, true);
+
+        public async Task MonitorConnection()
+        {
+            while (!(Client.Client.Poll(0, SelectMode.SelectRead) && Client.Client.Available == 0) && !CancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Delay(1000, CancellationToken);
+                }
+                catch (OperationCanceledException) { }
+            }
+
+            Disconnected?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
