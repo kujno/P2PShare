d301f76700ff3d25f13ea9684c1c3b65ac7604f4
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-24 23:21:04 +0100

    NewFolderWindow

---
+++ NewFolderWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace P2PShare
+{
+    /// <summary>
+    /// Interaction logic for ServerIPWindow.xaml
+    /// </summary>
+    public partial class NewFolderWindow : Window
+    {
+        public string? FolderName { get; private set; } = null;
+
+        public NewFolderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlockExit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Close();
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) DragMove();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TextBoxFolderName.Text.Trim();
+
+            if (name.All(x => Path.GetInvalidFileNameChars().All(y => y != x)))
+            {
+                FolderName = name;
+
+                Close();
+            }
+            else
+            {
+                TextBlockValid.Text = "Folder name contains invalid character(s).";
+                TextBlockValid.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
