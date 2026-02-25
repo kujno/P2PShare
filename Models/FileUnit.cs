180c0f2c85a968f39813ba5fdf215bb7412e975d
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-25 19:04:39 +0100

    FileUnit class

---
+++ Models/FileUnit.cs
@@ -0,0 +1,11 @@
+﻿using P2PShare.Libs.Models.FileSytem;
+
+namespace P2PShare.Models
+{
+    public class FileUnit
+    {
+        public required string Path { get; init; }
+        public required bool My { get; init; }
+        public required Unit Unit { get; init; }
+    }
+}
